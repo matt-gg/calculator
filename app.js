3a2fcdc2f2ee38f22b70875b1875363ebb213923
matt-gg <94838139+matt-gg@users.noreply.github.com>
2022-11-09 23:12:52 +1100

    Completed all basic calc and display functions

--- app.js
+++ app.js
@@ -1,82 +1,109 @@
+/*
+
+To-do list:
+    1. Round answers so they don't overflow the screen.
+    2. Disable decimal if there's already one in the display
+    3. Add a backspace button
+    4. Add keyboard support
+    5. Make it look nice!
+
+*/
+
+let calc = ['', '', ''];
+const operatorList = ['+', '-', '*', '/', '^x'];
+
+const screenLast = document.getElementById('screen-last');
+const screenCurrent = document.getElementById('screen-current');
+
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
-
-const piButton = document.getElementById('pi');
-const percentageButton = document.getElementById('percentage');
 
 const deleteButton = document.getElementById('delete');
 const clearButton = document.getElementById('clear');
 const equalsButton = document.getElementById('equals');
 
+
 numberButtons.forEach((button) => {
-    button.addEventListener('click', () => console.log(button.textContent));
+    button.addEventListener('click', () => buttonPress(button.textContent));
 });
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => console.log(button.textContent));
+    button.addEventListener('click', () => buttonPress(button.textContent));
 });
 
-piButton.addEventListener('click', () => console.log("pi"));
-percentageButton.addEventListener('click', () =>  console.log("%"));
-
-deleteButton.addEventListener('click', () =>  console.log("del"));
-clearButton.addEventListener('click', () =>  console.log("clear"));
-equalsButton.addEventListener('click', () =>  console.log("equals"));
+clearButton.addEventListener('click', () => clearProgram());
+deleteButton.addEventListener('click', () => console.log("delete"));
+equalsButton.addEventListener('click', () => calculation());
 
 
+function buttonPress(char) {
+    if (operatorList.includes(char)) {
+        if (calc[1] != '') {
+            calculation();
+            calc[1] = char;
+        } else {
+            calc[1] = char;
+        }
+    } else if (
+            typeof Number(char) === 'number' ||
+            char === '.') {
+        if (calc[1] === '') {
+            calc[0] += char;
+        } else {
+            calc[2] += char;
+        }
+    }
 
-function operation(operator, a, b) {
+    lastScreenRefresh();
+}
+
+function calculation() {
+    if (calc[1] === '/' && calc[2] === '0')
+        return console.log(`Hey! Stop trying to divide by 0!`);
+
+    const result = operation(calc[0], calc[1], calc[2])
+
+    if (calc.includes('')) return;
+    
+    console.log(`Result: ${result}`);
+
+    lastScreenRefresh();
+    currentScreenRefresh(result);
+    calc = [result, '', ''];
+}
+
+function lastScreenRefresh() {
+    screenLast.textContent = calc.join('');
+}
+
+function currentScreenRefresh(result) {
+    screenCurrent.textContent = result;
+}
+
+function clearProgram() {
+    calc = ['', '', ''];
+    lastScreenRefresh();
+    currentScreenRefresh();
+}
+
+function operation(a, operator, b) {
     a = Number(a);
     b = Number(b);
 
     switch(operator) {
         case "+":
+            return a + b;
             break;
         case "-":
+            return a - b;
             break;
         case "*":
+            return a * b;
             break;
         case "/":
+            return a / b;
             break
-        case "mod":
-            break;
-        case "root":
-            break;
-        case "^x:":
+        case "^x":
+            return Math.pow(a, b);
             break;
     }
-}
-
-// Math calculations
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function power(x) {
-    return Math.pow(x, 2);
-}
-
-function factorial(x) {
-    if (x === 0) return 1;
-    let total = 1;
-    for (let i = x; i > 0; i--) {
-        total *= i;
-    }
-    return total;
-}
-
-function root(x) {
-    return Math.sqrt(x);
 }
