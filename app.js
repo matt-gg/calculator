e43ce552b5566d8baec782bcdaa7265c5467d7e0
matt-gg <94838139+matt-gg@users.noreply.github.com>
2022-11-09 20:16:56 +1100

    Added EventListeners to buttons and confirmed they work

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+const piButton = document.getElementById('pi');
+const percentageButton = document.getElementById('percentage');
+
+const deleteButton = document.getElementById('delete');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => console.log(button.textContent));
+});
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => console.log(button.textContent));
+});
+
+piButton.addEventListener('click', () => console.log("pi"));
+percentageButton.addEventListener('click', () =>  console.log("%"));
+
+deleteButton.addEventListener('click', () =>  console.log("del"));
+clearButton.addEventListener('click', () =>  console.log("clear"));
+equalsButton.addEventListener('click', () =>  console.log("equals"));
+
+
+
+function operation(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    switch(operator) {
+        case "+":
+            break;
+        case "-":
+            break;
+        case "*":
+            break;
+        case "/":
+            break
+        case "mod":
+            break;
+        case "root":
+            break;
+        case "^x:":
+            break;
+    }
+}
+
 // Math calculations
 
 function add(a, b) {
